15cfc24a7e9117ca5b0a93f0387338cadde8933c
BenTachie <tachiebenedict@gmail.com>
2025-08-24 16:19:56 +0200

    feat: update the next.config.mjs file to integrate the next-pwa package

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public',
+});
+
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'], // allow movie images from IMDb
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+});
